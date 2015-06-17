5f59bc30038aa3f62fd61f3333eebdb586938e21
Cristian Cortés <cristian.cortes@brm.com.co>
2015-06-17 17:38:48 -0500

    ajuste para imprimir respuesta por ajax

--- casper/form.js
+++ casper/form.js
@@ -137,11 +137,12 @@ casper.then(function() {
 
 casper.then(function () {
     this.wait(5000, function() {
-            this.clear();
-            this.echo("5s");
-             casper.thenOpen("http://localhost:3000/respuesta.html", function() {
             this.capture('screenshots/04-submit-form-respuesta-server.jpg');
-       });
+       //      this.clear();
+       //      this.echo("5s");
+       //       casper.thenOpen("http://localhost:3000/respuesta.html", function() {
+       //      this.capture('screenshots/04-submit-form-respuesta-server.jpg');
+       // });
             this.echo('Page url is ' + this.getCurrentUrl());
             this.echo('Page title is ' + this.getTitle());
         
