0412eafdc63b7ef4976ece43a2849f2c535c9125
brm-benavidesa <andres.benavides@brm.com.co>
2015-06-23 22:44:40 -0500

    mesaje de alerta bonito

--- publication/js/funciones.js
+++ publication/js/funciones.js
@@ -1,17 +1,39 @@
-$(document).ready(function (){
-  
-  $("#Btnregistro").click(function(){
-    //if($("#Registro").valid()){
+$(document).ready(function () {
+  /*
+   * FUNCION PARA GENERAR ALERTA BONITA
+   */
+  var alerta = function (mensaje) {
+    $.blockUI({message: '<p id="mensajeAlert" >' + mensaje + ' </p> <br><button type="button" id="cerrar">Aceptar</button>',
+      css: {
+        cursor: 'pointer',
+        border: 'none',
+        padding: '15px',
+        backgroundColor: '#fff',
+        '-webkit-border-radius': '10px',
+        '-moz-border-radius': '10px',
+        opacity: .8,
+        color: '#fff'
+      }});
+    $("#cerrar").click(function () {
+      $.unblockUI();
+    });
+  };
+  $("#Btnregistro").click(function () {
+    if($("#Registro").valid()){
       $.ajax({
-        url:"guardaRegistro.php",
-        method:'post',
+        url: "guardaRegistro.php",
+        method: 'post',
         data: $("#Registro").serialize()
-      }).success(function(data){
-        console.log(data);
+      }).success(function (data) {
+        if(data==="registroOk"){
+          alerta("Registro guardado con éxito");
+        }else{
+          alerta("Error al guardar el registro");
+        }
       });
-   // }
+    }
   });
-  
+
 });
 
 
